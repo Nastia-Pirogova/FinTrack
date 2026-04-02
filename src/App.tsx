5239542dbf0117e3fd6727c7aab5290572cc129b
Anastasia Pirogova <pirogovaanastasia589@gmail.com>
2026-04-02 22:01:40 +0300

    feat: add and delete transactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import TransactionForm from './components/TransactionForm.tsx'
 import TransactionsList from './components/TransactionsList.tsx'
 import StatsCards from './components/StatsCards.tsx'
 import Header from "./components/Header.tsx";
+import {useState} from 'react'
 
 function App() {
+    const [transactions, setTransactions] = useState([]);
 
+    function deleteItem(id) {
+        return setTransactions(transactions.filter(item => item.id !== id))
+
+    }
 
     return (
         <>
@@ -14,8 +20,17 @@ function App() {
 
             <section id="transaction" className="transaction px-4 mb-4">
                 <div className="container mx-auto grid gap-8 xl:grid-cols-[1fr_2fr] grid-cols-1  justify-between ">
-                    <TransactionForm/>
-                    <TransactionsList/>
+                    <TransactionForm onSubmit={(values) => {
+                        setTransactions([
+                            ...transactions,
+                            {
+                                ...values,
+                                date: new Date(),
+                                id: Date.now()
+                            }
+                        ])
+                    }}/>
+                    <TransactionsList transactions={transactions} onDelete={deleteItem}/>
                 </div>
             </section>
 
